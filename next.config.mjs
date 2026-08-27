13c3f7e7bb8aba4932d0acd0ff6161e0e4448b3b
ssl-salman <salman@speedway-solutions.com>
2026-08-27 23:51:40 +0600

    add one site

--- next.config.mjs
+++ next.config.mjs
@@ -30,6 +30,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "skynellebeauty.com",
       },
+      {
+        protocol: "https",
+        hostname: "lavishta.com",
+      },
     ],
   },
 };
